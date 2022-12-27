4b7585563a2d9e7ef1352b536bdc2b9cb5aadfb4
Viktoriia Bieltsova <vikav0206@gmail.com>
2022-12-27 21:30:20 +0200

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsEL = document.getElementById("ingredients");
+
+const ingredientsListItem = list => {
+	return list.map(item => {
+		const ingredientsListEl = document.createElement("li");
+		ingredientsListEl.textContent = item;
+		ingredientsListEl.classList.add("item");
+		return ingredientsListEl;
+	});
+};
+
+const ingredientsItems = ingredientsListItem(ingredients);
+ingredientsEL.append(...ingredientsItems);
